255b46659865a006d111b65b7337ab27b7f57765
SteVasc <stefany.vasc.sa@gmail.com>
2020-01-21 20:28:44 -0300

    :construction: Start with javascript
    js for heart in description-product

---
+++ js/description-product.js
@@ -0,0 +1,15 @@
+/* 
+1 - Ter o browser na mão
+2 - Ter o HTML na mão
+3 - Pegar o coração
+4 - Pegar o clique no coração 
+5 - No momento que o usuário clicar mostrar um texto na tela
+*/
+
+alert("antes do comando que pega o coração!");
+
+const $heart = window.document.querySelector(".-heart");
+
+console.log($heart);
+
+alert("fim do arquivo");
