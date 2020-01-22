cc8fd9548a371390abb5c31149a356cecd78369a
Stefany <stefany.vasc.sa@gmail.com>
2020-01-22 16:03:26 -0200

    :poop:  Writing bad code that needs to be improved.
    try to resolve challenge intermediate 014

--- js/description-product.js
+++ js/description-product.js
@@ -6,10 +6,12 @@
 5 - No momento que o usuário clicar mostrar um texto na tela
 */
 
-const $heart = window.document.querySelector(".-heart");
+const $heart = document.querySelector(".-heart");
+const $star = document.querySelector(".-star");
 
 /* pegando o clique no heart */
 $heart.addEventListener("click", handleClick);
+$star.addEventListener("click", starClick);
 
 function handleClick() {
   /* if ($heart.classList.contains("-active")) {
@@ -18,4 +20,10 @@ function handleClick() {
     $heart.classList.add("-active"); // quando não tem a classe active
   } */
   $heart.classList.toggle("-active");
+}
+
+function starClick() {
+  const $oneStar = document.querySelectorAll(".star-img");
+  console.log($oneStar);
+  $oneStar.classList.toggle("-click");
 }
--- js/home.js
+++ js/home.js
@@ -5,6 +5,5 @@ $second.addEventListener("click", handleClick);
 
 function handleClick() {
   const $carrinho = document.querySelector(".-cart");
-  /* valorInicial++; */
   $carrinho.textContent = `Carrinho (${++valorInicial})`;
 }
