6300fe774f07836c6d102f0ca330fbcfb2870aaa
SteVasc <stefany.vasc.sa@gmail.com>
2020-01-21 22:50:45 -0300

    :construction: challenge advanced 013 done

--- js/button-store.js
+++ js/button-store.js
@@ -1,8 +1,11 @@
 const $cart = window.document.querySelector(".-second");
 
+var $textcart = window.document.querySelector(".-cart");
+
 $cart.addEventListener("click", handleClick);
 
 function handleClick() {
   console.log($cart, "foi clicado");
-  alert("Você quer mesmo adicionar ao carrinho");
+  /* alert("Você quer mesmo adicionar ao carrinho"); */
+  $textcart.textContent = "FUNFOU!";
 }
