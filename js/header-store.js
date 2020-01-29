881cd8797d697c5242cf9df18d500f51c33628ae
SteVasc <stefany.vasc.sa@gmail.com>
2020-01-29 03:29:51 -0300

    :poop: trying to resolve challenge intermediate 017

--- js/header-store.js
+++ js/header-store.js
@@ -1,5 +1,21 @@
 const $search = document.querySelector("[type=search]");
 
+const $searchOn = document.querySelector(".-on");
+const $searchOff = document.querySelector(".-close");
+
 $search.addEventListener("click", () => {
   $search.classList.toggle("-active");
+  $searchOn.classList.toggle("-active");
+  $searchOff.classList.toggle("-active");
 });
+
+console.log($searchOn.classList);
+console.log($searchOff.classList);
+
+/* $searchOn.addEventListener("click", () => {
+  $searchOn.classList.toggle("-active");
+});
+
+function iconClick() {
+  this.classList.toggle("-active");
+} */
