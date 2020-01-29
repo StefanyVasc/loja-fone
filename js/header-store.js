f64ac8a7ff9524396f3d6e56754f3d27303d9b46
Stefany <stefany.vasc.sa@gmail.com>
2020-01-29 12:13:35 -0200

    :construction: challenge intermediate 017 done

--- js/header-store.js
+++ js/header-store.js
@@ -1,21 +1,7 @@
-const $search = document.querySelector("[type=search]");
+const $iconSearch = document.querySelector(".search-icon");
+const $input = document.querySelector("[type=search]");
 
-const $searchOn = document.querySelector(".-on");
-const $searchOff = document.querySelector(".-close");
-
-$search.addEventListener("click", () => {
-  $search.classList.toggle("-active");
-  $searchOn.classList.toggle("-active");
-  $searchOff.classList.toggle("-active");
+$iconSearch.addEventListener("click", () => {
+  $input.classList.toggle("-active");
+  $iconSearch.classList.toggle("-active");
 });
-
-console.log($searchOn.classList);
-console.log($searchOff.classList);
-
-/* $searchOn.addEventListener("click", () => {
-  $searchOn.classList.toggle("-active");
-});
-
-function iconClick() {
-  this.classList.toggle("-active");
-} */
