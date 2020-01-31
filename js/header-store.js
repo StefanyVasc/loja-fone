d58d9a54b6ae00e878ac42f4591de47465215328
SteVasc <stefany.vasc.sa@gmail.com>
2020-01-30 23:45:41 -0300

    :recycle: fix all challenges 017

--- js/header-store.js
+++
@@ -1,7 +0,0 @@
-const $iconSearch = document.querySelector(".search-icon");
-const $input = document.querySelector("[type=search]");
-
-$iconSearch.addEventListener("click", () => {
-  $input.classList.toggle("-active");
-  $iconSearch.classList.toggle("-active");
-});
