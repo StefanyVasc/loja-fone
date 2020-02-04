57aec394224b1945d88bc000b9760d88089e4421
SteVasc <stefany.vasc.sa@gmail.com>
2020-02-04 03:50:43 -0300

    :recycle: fix all challenges 018

--- js/menu-hamburguer.js
+++
@@ -1,9 +0,0 @@
-const $menu = document.querySelector(".menu-hamburguer");
-const $navigation = document.querySelector(".navigation");
-const $links = $navigation.querySelectorAll(".action");
-
-$menu.addEventListener("click", () => {
-  $links.forEach($links => {
-    $links.classList.toggle("-active");
-  });
-});
